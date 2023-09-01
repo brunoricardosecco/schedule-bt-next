85ce999e5709382bfc6032d28289b31f1aacf86d
Bruno Ricardo Secco <brunoricardosecco@hotmail.com>
2023-08-31 21:00:07 -0300

    feat: change pages title

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -199,7 +199,7 @@ export const Layout = () => {
             <div className="py-6">
               <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <h1 className="text-2xl font-semibold text-gray-900">
-                  Dashboard
+                  {navigationPaths.find((path) => path.isActive)?.name || ""}
                 </h1>
               </div>
               <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
